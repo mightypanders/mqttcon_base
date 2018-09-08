5c9822b30860476ec7f1a600ee45ce997afdfe6c
markus <markus@endor>
2018-09-08 20:49:17 +0200

    fs start

---
+++ src/filesystem.h
@@ -0,0 +1,22 @@
+#ifndef filesystem_H
+#define filesystem_H
+#include <FS.h>
+
+class filesystem
+{
+
+    private:
+    public:
+    filesystem();
+    void open();
+    
+};
+
+typedef struct {
+
+string ssid;
+string pass;
+
+} wificreds
+
+#endif
